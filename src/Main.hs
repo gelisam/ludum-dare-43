e8e805d8b58e28d60407eed5edd935d7d9707c6a
Samuel Gélineau <gelisam+github@gmail.com>
2018-11-30 09:05:51 -0500

    gloss + sdl2-mixer demo

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,34 @@
 module Main where
 
+import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Game
+import System.Exit
+import qualified SDL as SDL
+import qualified SDL.Mixer as Audio
+
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  SDL.initialize [SDL.InitAudio]
+  Audio.withAudio Audio.defaultAudio 256 $ do
+    coin1 <- Audio.load "assets/sounds/coin.wav"
+    playIO (InWindow "Nice Window" (200, 200) (10, 10))
+           white
+           30
+           ()
+           draw
+           (reactEvent coin1)
+           reactTime
+
+draw :: () -> IO Picture
+draw () = pure $ circle 80
+
+reactEvent :: Audio.Chunk -> Event -> () -> IO ()
+reactEvent coin1 event () = case event of
+  EventKey (Char 'q')          Down _ _ -> exitSuccess
+  EventKey (SpecialKey KeyEsc) Down _ _ -> exitSuccess
+  EventKey _                   Down _ _ -> Audio.play coin1
+  _                                     -> pure ()
+
+reactTime :: Float -> () -> IO ()
+reactTime _ () = pure ()
